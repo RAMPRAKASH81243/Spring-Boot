191bffc1ab0937caf5e822ead7262690a48db938
RAMPRAKASH81243 <68095922+RAMPRAKASH81243@users.noreply.github.com>
2024-09-13 16:46:03 +0530

    Update Alien.java

--- SpringBootFrist DI/src/main/java/com/ram/app/Alien.java
+++ SpringBootFrist DI/src/main/java/com/ram/app/Alien.java
@@ -1,11 +1,10 @@
 package com.ram.app;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Alien {
-	@Autowired
+	
 	Laptop laptop;
 	public void code() {
 		laptop.compile();
