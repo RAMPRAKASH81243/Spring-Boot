ee35ce1324e3e8582709f6eddcdd515ff66ddfb5
RAMPRAKASH81243 <68095922+RAMPRAKASH81243@users.noreply.github.com>
2024-09-13 12:05:52 +0530

    Add files via upload

---
+++ SpringBootFrist/src/main/java/com/ram/app/SpringBootFristApplication.java
@@ -0,0 +1,14 @@
+package com.ram.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFristApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFristApplication.class, args);
+		System.out.println("hello world");
+	}
+
+}
