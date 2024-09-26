66dbba5caa37e6c5e9681c1a74d95fd04618bd11
RAMPRAKASH81243 <68095922+RAMPRAKASH81243@users.noreply.github.com>
2024-09-26 11:39:42 +0530

    Add files via upload

---
+++ Spring Boot Web /5.15 Model Object/src/main/java/com/ram/SpringBootweb1/HomeController.java
@@ -0,0 +1,26 @@
+package com.ram.SpringBootweb1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String home() {
+		return "index.jsp";
+	}
+	
+	@RequestMapping("add")
+	public String add(@RequestParam("num1")int a, @RequestParam("num2")int b, Model model) {
+		
+		int result= (a+b)*2;
+		model.addAttribute("result", result);
+		return "result.jsp";
+	}
+}
